0f26a405cd7adc9b93a917dd63071c2e8b8275f4
Himanshu Anand <work.anandhimanshu@gmail.com>
2024-05-16 13:18:19 +0530

    completed

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Calculator App Project - [live=link]()
+ Welcome to the repository for my Calculator App project completed as part of the Oasis Infobyte Web Development Internship! This project aims to create a user-friendly calculator application with basic arithmetic operations.
+
+# Features:
+ Arithmetic Operations: Perform addition, subtraction, multiplication, and division.
+ User Interface: Intuitive interface for seamless user experience.
+ Error Handling: Robust error handling to ensure accurate  calculations.
+ Responsive Design: Compatible across various devices and screen sizes.
+
+# Technologies Used:
+ HTML/CSS: Frontend development for the user interface.
+ JavaScript: Backend logic for implementing calculations and functionality.
+ Git/GitHub: Version control and collaboration.
+
+
+I'm excited to share my progress and continue refining this project as I advance through my internship journey. Feel free to explore the code and provide feedback or suggestions! 🚀
---
+++ script.js
@@ -0,0 +1,123 @@
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+    this.updateDisplay();
+  }
+
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    this.updateDisplay();
+  }
+
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+    this.updateDisplay();
+  }
+
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+    this.updateDisplay();
+  }
+
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "*":
+        computation = prev * current;
+        break;
+      case "÷":
+        if (current === 0) {
+          computation = "Invalid";
+        } else {
+          computation = prev / current;
+        }
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = "";
+    this.updateDisplay();
+  }
+
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0,
+      });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+
+  updateDisplay() {
+    if (this.currentOperand === "Invalid") {
+      this.currentOperandTextElement.innerText = "Invalid";
+    } else {
+      this.currentOperandTextElement.innerText = this.getDisplayNumber(
+        this.currentOperand
+      );
+    }
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
+        this.previousOperand
+      )} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = "";
+    }
+  }
+}
+
+const calculator = new Calculator(
+  document.querySelector("[data-previous-operand]"),
+  document.querySelector("[data-current-operand]")
+);
+
+document.addEventListener("click", (event) => {
+  const { target } = event;
+  if (target.matches("[data-number]")) {
+    calculator.appendNumber(target.innerText);
+  } else if (target.matches("[data-operation]")) {
+    calculator.chooseOperation(target.innerText);
+  } else if (target.matches("[data-equals]")) {
+    calculator.compute();
+  } else if (target.matches("[data-all-clear]")) {
+    calculator.clear();
+  } else if (target.matches("[data-delete]")) {
+    calculator.delete();
+  }
+});
